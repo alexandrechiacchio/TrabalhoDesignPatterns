d1dd27ce68e84c4d5dae6cee821de4dc7fce1046
Alexandre Chiacchio <alexandrechiacchio30@gmail.com>
2024-07-01 17:36:11 -0300

    changed paths on IIFE

--- IIFE.cpp
+++ IIFE.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include "tinyxml2.h"
-#include "tinyxml2.cpp"
+#include "utils/tinyxml2.h"
+#include "utils/tinyxml2.cpp"
 
 int main() {
 
   // exemplo a ser substituido: quer-se ler de um xml
   tinyxml2::XMLDocument doc;
-  doc.LoadFile( "/mnt/d/UFRJ/ProgramacaoAvancada/example.xml" );
+  doc.LoadFile( "utils/example.xml" );
 
   const char *data1 = doc.FirstChildElement("document")->FirstChildElement("data")->GetText();
 
   std::cout << data1 << '\n';
 
   // mas usando IIFE
-  const char *data2 = [](const char* file = "/mnt/d/UFRJ/ProgramacaoAvancada/example.xml"){
+  const char *data2 = [](const char* file = "utils/example.xml"){
 
     tinyxml2::XMLDocument doc1;
 	  doc1.LoadFile( file );
